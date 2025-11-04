3fa2034f48115f7634f834b6705dd56711623446
Nestor Demeure <nestordemeure@gmail.com>
2025-11-04 12:43:34 -0800

    made it work offline

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'grocery-list-v1';
+const urlsToCache = [
+  '/grocery-list/',
+  '/grocery-list/index.html'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
